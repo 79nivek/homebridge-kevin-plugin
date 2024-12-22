2987864951ba6315cd2dac8f239c04f934a2ca02
quangnam <quangnam.nguyen@mati.com.vn>
2024-12-22 14:04:36 +0700

    chore(release): publish

--- libs/ac/src/index.ts
+++ libs/ac/src/index.ts
@@ -8,4 +8,5 @@ import { AcAccessory } from './lib/HeaterCoolerAccessory';
 export = (api: API) => {
   // api.registerPlatform(PLATFORM_NAME, KevinHomebridgePlatform);
   api.registerAccessory('homebridge-plugin-kevin', 'ac', AcAccessory);
+  console.log('ac plugin loaded');
 };
