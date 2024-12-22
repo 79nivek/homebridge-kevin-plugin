9ae237480efd80ade89cbbdad8cd5b016049d1f4
quangnam <quangnam.nguyen@mati.com.vn>
2024-12-22 13:52:48 +0700

    chore(release): publish

--- libs/ac/src/index.ts
+++ libs/ac/src/index.ts
@@ -1,13 +1,11 @@
-import { API } from "homebridge";
+import { API } from 'homebridge';
 
-import { FanAccessory } from "./lib/FanAccessory";
-import { AcAccessory } from "./lib/HeaterCoolerAccessory";
+import { AcAccessory } from './lib/HeaterCoolerAccessory';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
   // api.registerPlatform(PLATFORM_NAME, KevinHomebridgePlatform);
-  api.registerAccessory("homebridge-plugin-kevin", "fan", FanAccessory);
-  api.registerAccessory("homebridge-plugin-kevin", "ac", AcAccessory);
+  api.registerAccessory('homebridge-plugin-kevin', 'ac', AcAccessory);
 };
