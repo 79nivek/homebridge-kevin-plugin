80d9de13a8696195451c35508c6a6740bcc52f57
quangnam <quangnam.nguyen@mati.com.vn>
2024-12-23 00:06:55 +0700

    refactor(settings): update string quotes and add new plugin constants

--- libs/ac/src/lib/settings.ts
+++ libs/ac/src/lib/settings.ts
@@ -1,11 +1,17 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = "@79nivek/homebridge-ac";
+export const PLATFORM_NAME = '@79nivek/homebridge-ac';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = "@79nivek/homebridge-ac";
+export const PLUGIN_NAME = '@79nivek/homebridge-ac';
 
-export const PLUGIN_CONFIG_SCHEMA = "config.schema.json";
+export const PLUGIN_CONFIG_SCHEMA = 'config.schema.json';
+
+export const PLUGIN_PACKAGE_JSON = 'package.json';
+
+export const PLUGIN_README_MD = 'README.md';
+
+export const PLUGIN_CHANGELOG_MD = 'CHANGELOG.md';
