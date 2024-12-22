640c62ee721c41440158272b63b2fc147f32cf4f
quangnam <quangnam.nguyen@mati.com.vn>
2024-12-23 00:09:17 +0700

    chore(project): enhance project.json configuration and clean up settings.ts

--- libs/ac/src/lib/settings.ts
+++ libs/ac/src/lib/settings.ts
@@ -7,11 +7,3 @@ export const PLATFORM_NAME = '@79nivek/homebridge-ac';
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = '@79nivek/homebridge-ac';
-
-export const PLUGIN_CONFIG_SCHEMA = 'config.schema.json';
-
-export const PLUGIN_PACKAGE_JSON = 'package.json';
-
-export const PLUGIN_README_MD = 'README.md';
-
-export const PLUGIN_CHANGELOG_MD = 'CHANGELOG.md';
