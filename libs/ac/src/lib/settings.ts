025ae34fb64678c915b1909316060600dd7dc6c0
quangnam <quangnam.nguyen@mati.com.vn>
2024-12-23 02:18:13 +0700

    chore(project): remove 'push' option from project.json configuration

--- libs/ac/src/lib/settings.ts
+++ libs/ac/src/lib/settings.ts
@@ -9,5 +9,3 @@ export const PLATFORM_NAME = '@79nivek/homebridge-ac';
 export const PLUGIN_NAME = '@79nivek/homebridge-ac';
 
 export const PLUGIN_VERSION = '0.1.1';
-
-export const PLUGIN_NAME_AC = 'ac';
